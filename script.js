69e389d1894a38e8075d7fdbedb90c3d4c408006
Youssef Tamer <102515861+UZRIII@users.noreply.github.com>
2024-06-26 23:07:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const welcomeText = document.querySelector('.welcome');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+    welcomeText.classList.add('hidden');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+    welcomeText.classList.remove('hidden');
+});
